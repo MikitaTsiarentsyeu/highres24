d24cc18b063ef2e5d642aabd8051b6a6a6c578c7
sadylodr <yagor.rytvinski@stud.esdc.lt>
2025-03-17 11:41:27 +0200

    fix

--- assignments/24-HR-CS2/homework 2/rytvinski/main.py
+++ assignments/24-HR-CS2/homework 2/rytvinski/main.py
@@ -1,5 +1,5 @@
 def calculate(starting_sum, rate, period_in_years):
-    return starting_sum * (1 + rate / 100) * period_in_years
+    return starting_sum * (1 + rate / 100) ** period_in_years
 
 
 print("Income Calculator")
@@ -8,7 +8,7 @@ try:
     rate = float(input("Enter the interest rate (%): "))
     period = int(input("Enter the number of years: "))
     
-    result = calculate(starting_sum, rate, period) - starting_sum;
+    result = calculate(starting_sum, rate, period) - starting_sum
     
     print(f"Income after {period} years is {round(result, 2)}")
 except ValueError:
