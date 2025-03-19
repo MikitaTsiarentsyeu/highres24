96a6dc6afeb6206f56078bffcf0029ab106e1f73
Egor Koptev <george.hirshfield@gmail.com>
2025-03-19 18:54:42 +0200

    Koptev-Hometask-3
    Egor Koptev

---
+++ homework-3/task2.py
@@ -0,0 +1,20 @@
+inputStr = input("Enter a equation: ")
+inputStr = inputStr.replace("x", "")
+inputStr = inputStr.replace("\\", "")
+inputStr = inputStr.replace("=0", "")
+inputStr = inputStr.split("+")
+a = int(inputStr[0])
+b = int(inputStr[1])
+c = int(inputStr[2])
+
+discriminant = b**2 - 4*a*c
+if discriminant > 0:
+    rootPlus = (-b + (discriminant ** 0.5)) / (2 * a)
+    rootMinus = (-b - (discriminant ** 0.5)) / (2 * a)
+    print("Answer 1: ", rootPlus, "\nAnswer 2: ", rootMinus)
+elif discriminant == 0:
+    root = -b / (2 * a)
+    print("Answer: ",root)
+else:
+    print("Error")
+#EgorKoptev
---
+++ homework-3/task3.py
@@ -0,0 +1,35 @@
+time = input("Enter time (hh:mm): ")
+
+words = {
+        1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven",
+        8: "eight", 9: "nine", 10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen",
+        14: "fourteen", 15: "quarter", 16: "sixteen", 17: "seventeen", 18: "eighteen",
+        19: "nineteen", 20: "twenty", 30: "thirty", 40: "forty", 50: "fifty"
+    }
+
+hours, minutes = map(int, time.split(":"))
+
+if hours == 0 and minutes == 0:
+    print("midnight")
+    exit()
+
+if hours > 12:
+    hours -= 12
+
+if minutes == 0:
+    print(f"{words[hours]} o'clock")
+elif minutes == 15:
+    print(f"quarter past {words[hours]}")
+elif minutes == 30:
+    print(f"half past {words[hours]}")
+elif minutes == 45:
+    next_hour = hours + 1
+    if next_hour > 12:
+        next_hour = 1
+    print(f"quarter to {words[next_hour]}")
+elif minutes < 30:
+    print(f"{words[minutes]} past {words[hours]}")
+else:
+    next_hour = hours + 1 if hours < 12 else 1
+    print(f"{words[60 - minutes]} to {words[next_hour]}")
+#EgorKoptev
