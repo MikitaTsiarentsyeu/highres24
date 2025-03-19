e1bfd104db3b14208886383380dac02acd018b69
Egor Koptev <george.hirshfield@gmail.com>
2025-03-19 18:07:30 +0200

    Koptev-Hometask-2
    Egor Koptev

--- assignments/24-HR-CS1/homework 2/isaevi-homework/homework-02.py
+++
@@ -1,5 +0,0 @@
-p = float(input("starting sum:"))
-a = float(input("procents (%): "))
-r = float(input(" invest period :"))
-print("result:", p*(1+a/100)**r)
-
--- homework-2/homework.py
+++ homework-2/homework.py
@@ -1,1 +1,6 @@
-print("Hello world!")
+startNum = int(input("Enter a start number: "))
+interestRate = int(input("Enter an interest rate (%): ")) / 100
+period = int(input("Enter a period (year): "))
+income = startNum * interestRate * period
+print("The income is:", income)
+#EgorKoptev
