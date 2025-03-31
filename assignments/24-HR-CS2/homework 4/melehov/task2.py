0a0b78c85a66e87c4a694f973a020ef88ba514b7
Anton Melehov <ax7704@gmail.com>
2025-03-31 21:29:59 +0300

    melehov: hm 4

---
+++ assignments/24-HR-CS2/homework 4/melehov/task2.py
@@ -0,0 +1,10 @@
+def bubble_sort(lst):
+    n = len(lst)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+    return lst
+
+l = [2,56,3,2,4,56,8,9]
+print(bubble_sort(l))
