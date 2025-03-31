d3093770fc5b4e3e983f210f75af8b197a01ba20
edrdavid1 <murashka.david@student.ehu.lt>
2025-03-31 11:00:46 +0300

    HW4

---
+++ assignments/24-HR-JS/murashka_hw_4/task_2.py
@@ -0,0 +1,15 @@
+def optimized_bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        swapped = False
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+print(optimized_bubble_sort(arr))
