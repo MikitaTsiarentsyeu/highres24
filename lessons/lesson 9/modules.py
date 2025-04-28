ca86f66eddec5990f6c3034d0386754b38cdc595
MikitaTsiarentsyeu <terentievnikita@gmail.com>
2025-04-28 10:17:48 +0300

    add lesson 9

---
+++ lessons/lesson 9/modules.py
@@ -0,0 +1,13 @@
+import test_lib as tl
+# from test_lib import test_func as tl, test_var as tv
+# from test_lib import *
+
+from decimal import Decimal as D
+
+def test_func():
+    print("test")
+
+tl.test_func()
+
+
+
---
+++ lessons/lesson 9/test_lib.py
@@ -0,0 +1,9 @@
+test_var = "some str object"
+
+def test_func():
+    print(test_var)
+
+print(__name__)
+
+if __name__ == "__main__":
+    print("test?")
