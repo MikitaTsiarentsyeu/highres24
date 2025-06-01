ed670826849e587cb7fc6b3b892aacb8b4b06cff
DaNihto <thethought43@gmail.com>
2025-06-02 00:34:18 +0300

    Create 11-animals.py

---
+++ assignments/24-HR-JS/Verkhovodko/11-animals.py
@@ -0,0 +1,27 @@
+class Animal:
+    def move():
+        print("default moving")
+
+    def sound():
+        print("seems silent")
+
+class Bird(Animal):
+    def move():
+        print("flying")
+      
+    def sound():
+        print("chirp")
+
+class Mammal(Animal):
+    def move():
+        print("walking")
+      
+    def sound():
+        print("mammal sounds")
+
+class Fish(Animal);
+    def move():
+        print("swimming")
+      
+    def sound():
+        print("blurp")
